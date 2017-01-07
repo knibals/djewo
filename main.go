1d2ccd99a317030369a6bb699a7402ff57872777
Oumar FALL <oumar.fall@orange-sonatel.com>
2017-01-07 21:44:19 +0000

    A minimal main file to test Heroku deployment

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatalln("Le numéro de port est nécessaire")
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Bonjour le monde")
+	})
+	log.Printf("Serveur démarré sur http://localhost:%s...\n", port)
+	http.ListenAndServe("localhost:"+port, nil)
+}
